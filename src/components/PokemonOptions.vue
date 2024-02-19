498a421ac74bf47ff560247be0e300cc1546b9ab
Martín Núñez <mnunez@linkbgroup.com>
2024-02-19 16:25:31 -0600

    chore | Estructura final de la aplicación de Pokemon

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,13 +1,46 @@
 <template>
-  <h3>Pokémon options</h3>
+  <div class="options-container">
+    <ul>
+        <li v-for="pokemon in pokemonGroup" 
+            :key="pokemon.id"
+            @click="$emit('selection', pokemon.id)" >
+            {{ pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}}
+        </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-
+    props: {
+        pokemonGroup: {
+            type: Array,
+            required: true
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+    ul {
+        list-style-type: none;
+    }
 
+    li {
+        background-color: white;
+        border-radius: 5px;
+        border: 1px solid rgba(0, 0, 0, 0.2);
+        cursor: pointer;
+        margin-bottom: 10px;
+        width: 250px;
+    }
+
+    li:hover {
+        background-color: rgba(0, 0, 0, 0.05);
+    }
+
+    .options-container {
+        display: flex;
+        justify-content: center;
+    }
 </style>
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,17 +1,75 @@
 <template>
-  <h1>¿Quién es este Pokémon?</h1>
-  <PokemonPicture />
-  <PokemonOptions />
+    <h1 v-if="!pokemon">Cargando...</h1>
+    <div v-else>
+
+        <h1>¿Quién es este Pokémon?</h1>
+
+        <PokemonPicture 
+            :pokemonId="pokemon.id" 
+            :showPokemon="showPokemon" 
+        />
+
+        <PokemonOptions 
+            :pokemonGroup="pokemonArr" 
+            @selection="checkAnswer" 
+        />
+
+        <div v-if="showAnswer">
+            <h2 class="fade-in">{{ message }}</h2>
+            <button @click="newGame">
+                Nuevo juego
+            </button>
+        </div>
+
+    </div>
 </template>
 
 <script>
 import PokemonPicture from '@/components/PokemonPicture.vue'
 import PokemonOptions from '@/components/PokemonOptions.vue'
+import getPokemonOptions from '@/helpers/getPokemonOptions'
 
 export default {
     components: {
         PokemonPicture,
         PokemonOptions
+    },
+    data() {
+        return {
+            pokemonArr: [],
+            pokemon: null,
+            showPokemon: false,
+            showAnswer: false
+        }
+    },
+    methods: {
+        async mixPokemonArray() {
+            this.pokemonArr = await getPokemonOptions()
+
+            const randInt = Math.floor( Math.random() * 4)
+            this.pokemon = this.pokemonArr[ randInt ]
+        },
+        checkAnswer(selectedId) {
+            this.showPokemon = true
+            this.showAnswer = true
+
+            if (selectedId === this.pokemon.id) {
+                this.message = `¡Correcto, es ${ this.pokemon.name.charAt(0).toUpperCase() + this.pokemon.name.slice(1) }!`
+            } else {
+                this.message = `Incorrecto, el Pokémon es ${ this.pokemon.name.charAt(0).toUpperCase() + this.pokemon.name.slice(1) }`
+            }
+        },
+        newGame() {
+            this.showPokemon = false
+            this.showAnswer = false
+            this.pokemonArr = []
+            this.pokemon = null
+
+            this.mixPokemonArray()
+        }
+    },
+    mounted() {
+        this.mixPokemonArray()
     }
 }
 </script>
